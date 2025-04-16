32cecf4eb4f46f85c0d4ee804a1140ff8e65e913
Evgenii Guguchkin <eugene.guguchkin@gmail.com>
2025-04-16 12:16:06 +0300

    Fix nil qpr for empty frac serach

--- searcher/searcher_test.go
+++ searcher/searcher_test.go
@@ -1,12 +1,15 @@
 package searcher
 
 import (
+	"context"
 	"errors"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/ozontech/seq-db/consts"
 	"github.com/ozontech/seq-db/frac"
+	"github.com/ozontech/seq-db/parser"
 	"github.com/ozontech/seq-db/seq"
 )
 
@@ -16,6 +19,14 @@ type testFakeFrac struct {
 
 func (t *testFakeFrac) IsIntersecting(_, _ seq.MID) bool {
 	return true
+}
+
+func (t *testFakeFrac) DataProvider(_ context.Context) (frac.DataProvider, func(), bool) {
+	return nil, nil, false
+}
+
+func (t *testFakeFrac) Info() *frac.Info {
+	return frac.NewInfo("test", 0, 0)
 }
 
 func TestFracsLimit(t *testing.T) {
@@ -32,4 +43,25 @@ func TestFracsLimit(t *testing.T) {
 	_, err := searcher.prepareFracs(testFracs, Params{})
 	assert.Error(t, err)
 	assert.True(t, errors.Is(err, consts.ErrTooManyFractionsHit))
+}
+
+func TestEmptyFracs(t *testing.T) {
+	searcher := New(1, Conf{})
+
+	ctx := context.Background()
+
+	seqql, err := parser.ParseSeqQL("level:7", seq.TestMapping)
+	assert.NoError(t, err)
+
+	params := Params{
+		AST:   seqql.Root,
+		From:  seq.MID(0),
+		To:    seq.MID(math.MaxUint64),
+		Limit: 100,
+	}
+
+	qpr, err := searcher.SearchDocs(ctx, frac.List{&testFakeFrac{}}, params)
+	assert.NoError(t, err)
+
+	assert.Empty(t, qpr.IDs)
 }
